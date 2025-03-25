15db7fbfc1386bd60d5fb89d1fd1634fc7ac957a
AhmedWaleedKalifa <ahmed2210waleed@gmail.com>
2025-03-25 18:40:04 +0200

    attach with the databasee from the host

--- db/populatedb.js
+++ db/populatedb.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
-
+const fs = require('fs');
+const path=require("node:path")
 const {Client}=require("pg");
 
 const SQL=`
@@ -17,8 +18,12 @@ VALUES
 async function main(){
     console.log("running");
     const client =new Client({
-        connectionString:process.env.CONNECTION_STRING
-    })
+        connectionString: `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+        ssl: {
+          ca: fs.readFileSync(path.resolve(process.env.DB_SSL_CERT)).toString(),
+          rejectUnauthorized: true 
+        }
+      })
     await client.connect();
     await client.query(SQL);
     await client.end();
