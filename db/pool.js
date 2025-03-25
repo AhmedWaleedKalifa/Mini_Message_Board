2f12e9e73969c834564bd2dedee114dbe6791de1
AhmedWaleedKalifa <ahmed2210waleed@gmail.com>
2025-03-25 18:21:33 +0200

    connect to production data base

--- db/pool.js
+++ db/pool.js
@@ -4,32 +4,11 @@ const path=require("node:path")
 const fs = require('fs');
 require("dotenv").config();
 const pool = new Pool({
-  connectionString: `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_PORT}?sslmode=require`,
-  ssl: { ca: fs.readFileSync(process.env.DB_SSL_CERT).toString() }
-});
+    connectionString: `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+    ssl: {
+      ca: fs.readFileSync(path.resolve(process.env.DB_SSL_CERT)).toString(),
+      rejectUnauthorized: true 
+    }
+  });
 
 module.exports = pool;
-console.log("CA cert path:", path.resolve(process.env.DB_SSL_CERT));
-
-
-const client = new Client({
-  connectionString: `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
-  ssl: {
-    ca: fs.readFileSync(path.resolve(process.env.DB_SSL_CERT)).toString(),
-    rejectUnauthorized: true // Keep this true for security
-  }
-});
-client.connect(function (err) {
-    if (err)
-        throw err;
-    client.query("SELECT VERSION()", [], function (err, result) {
-        if (err)
-            throw err;
-
-        console.log(result.rows[0].version);
-        client.end(function (err) {
-            if (err)
-                throw err;
-        });
-    });
-});
